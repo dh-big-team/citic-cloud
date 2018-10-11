c93ef0f3d01c82beaf2cdd0cca942632600f356f
zdclovejava <zdclovejava@163.com>
2018-10-11 11:33:12 +0800

    添加ReqParamUtil类，增加将json字符串转为一维结构的map方法

---
+++ src/main/java/com/dhcc/citic/cloud/utils/ReqParamUtil.java
@@ -0,0 +1,77 @@
+package com.dhcc.citic.cloud.utils;
+
+import java.util.HashMap;
+import java.util.Set;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * 腾讯侧api请求参数转换类
+ * 文件名称:     ReqParamUtil.java
+ * 内容摘要: 
+ * @author:   Zeng Dongcheng
+ * @version:  1.0  
+ * @Date:     2018年10月11日上午11:31:22 
+ * 
+ * 修改历史:  
+ * 修改日期                     修改人员                                   版本	            修改内容  
+ * ----------------------------------------------  
+ * 2018年10月11日     Zeng Dongcheng   1.0     新建
+ *
+ * 版权:   版权所有(C)2018
+ * 公司:   东华云计算有限公司
+ */
+public class ReqParamUtil {
+	
+	/**
+	 * 将json字符串转为腾讯api要求格式的map
+	 * @param jsonStr
+	 * @return
+	 */
+	public static void jsonStrToMap(HashMap<String,String> paramMap,String jsonStr){
+		JSONObject json = JSONObject.parseObject(jsonStr);
+		Set<String> it = json.keySet();
+		for(String key : it){
+			Object obj = json.get(key);
+			if(obj instanceof JSONObject || obj instanceof JSONArray){
+				ReqParamUtil.relaxJsonStr(obj, key, paramMap); 
+			}else{
+				paramMap.put(key, String.valueOf(obj));
+			}
+		}
+		 
+	}
+	
+	/**
+	 * 递归解析Json对象
+	 * @param sourceJson
+	 * @param prefix
+	 * @param param
+	 */
+	private static void relaxJsonStr(Object sourceJson,String prefix,HashMap<String,String> param){
+		if(sourceJson instanceof JSONObject){
+			JSONObject jsonObject = (JSONObject)sourceJson;
+			Set<String> it = jsonObject.keySet();
+			for(String key : it){
+				Object obj = jsonObject.get(key);
+				if(obj instanceof JSONObject || obj instanceof JSONArray){
+					
+					relaxJsonStr(obj,prefix+"."+key,param);
+				}else{
+					param.put(prefix+"."+key, String.valueOf(obj));
+				}
+			}
+		}else{
+			JSONArray jsonArray = (JSONArray) sourceJson;
+			int i=0;
+			for(Object jsonObject : jsonArray){
+				if(jsonObject instanceof JSONObject || jsonObject instanceof JSONArray){
+					relaxJsonStr(jsonObject,prefix+"."+i,param);
+				}else{
+					param.put(prefix+"."+i,String.valueOf(jsonObject));
+				}
+				++i;
+			}
+		}
+	}
+}
--- src/test/java/com/example/bootdemo/Test.java
+++ src/test/java/com/example/bootdemo/Test.java
@@ -1,12 +1,23 @@
 package com.example.bootdemo;
 
-import org.apache.shiro.crypto.hash.SimpleHash;
-import org.apache.shiro.util.ByteSource;
+import java.util.HashMap;
+import com.dhcc.citic.cloud.utils.ReqParamUtil;
 
 public class Test {
 	
 	public static void main(String args[]){
-		String newPassword = new SimpleHash("md5", "123456", ByteSource.Util.bytes("123"), 1).toHex();
-		System.out.println(newPassword);
+		//String newPassword = new SimpleHash("md5", "123456", ByteSource.Util.bytes("123"), 1).toHex();
+		//System.out.println(newPassword);
+		
+		 String jsonStr = "{\"Placement\":{\"Zone\":\"ss\",\"ProjectId\":\"ss\",\"HostIds\":[\"sdf\",\"df\"]},\"HostChargePrepaid\":{\"Period\":\"fff\",\"RenewFlag\":\"fff\"},\"HostChargeType\":\"ffff\",\"HostType\":\"dddd\"}";
+		 System.out.println(jsonStr);
+		 HashMap<String,String> paramMap = new HashMap<String,String>();
+		 ReqParamUtil.jsonStrToMap(paramMap,jsonStr);
+		 for(String key : paramMap.keySet()){
+			 System.out.println(key+"："+paramMap.get(key));
+		 }
+		 
 	}
+	
+	
 }
