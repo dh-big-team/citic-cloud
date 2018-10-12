de744b744791915c6d3c00899e0ce3e5ce1e2124
duanzhangquan <691240951>
2018-10-12 16:42:34 +0800

    查询云硬盘实例列表的接口和测试接口请求

---
+++ src/main/java/com/dhcc/citic/cloud/service/CbsService.java
@@ -0,0 +1,14 @@
+package com.dhcc.citic.cloud.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.dhcc.citic.cloud.common.BaseResult;
+import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+
+public interface CbsService {
+	/**
+	 * 查询云硬盘实例列表
+	 * @return
+	 * @throws TencentCloudSDKException
+	 */
+	public BaseResult citicDescribeDisks(String serviceId,String userId,JSONObject jsonParam) throws TencentCloudSDKException;
+}
---
+++ src/main/java/com/dhcc/citic/cloud/service/impl/CbsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.dhcc.citic.cloud.service.impl;
+import java.util.HashMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.alibaba.fastjson.JSONObject;
+import com.dhcc.citic.cloud.common.BaseResult;
+import com.dhcc.citic.cloud.config.QcloudConfig;
+import com.dhcc.citic.cloud.req.ApiRequest;
+import com.dhcc.citic.cloud.service.CbsService;
+import com.dhcc.citic.cloud.utils.ReqParamUtil;
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+@Service("cbsService")
+public class CbsServiceImpl implements  CbsService{
+
+	@Autowired
+	QcloudConfig qcloudConfig;
+	@Override
+	public BaseResult citicDescribeDisks(String serviceId, String userId, JSONObject jsonParam)throws TencentCloudSDKException {
+		//将实体转化成MAP，主要是将复杂结构的数据的key转化成key1.index.key2等，如DataDisks.0.DiskType=LOCAL_BASIC
+		HashMap<String, String> reqMap = new HashMap<String, String>();
+		ReqParamUtil.jsonObjectToMap(reqMap, jsonParam);
+		//生成腾讯鉴权
+		Credential cred = new Credential(qcloudConfig.getSecretId(), qcloudConfig.getSecretKey());
+		//根据商品ID，取得腾讯API对应接口，如需V2接口，则加参数flag=2,
+		String endPoint =   "cbs.tencentcloudapi.com";  // TencentCloudUrlConfig.getEndPoint(serviceId, 3);
+		// 
+		ApiRequest req = new ApiRequest(endPoint,cred,reqMap.get("Region"));
+		//调用腾讯接口
+		JSONObject rep = req.recvResponseRequest(reqMap, "DescribeDisks");
+		return new BaseResult(rep);
+	}
+
+}
